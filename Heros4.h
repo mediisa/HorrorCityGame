46df6db61ce25f56b40f8b9c5d8847d5f41f3596
mediisa <mediisa598@gmail>
2026-07-02 15:41:07 +0330

    Add Hero classes: Archaeologist, Mayor, Scientist, Courier with special actions

---
+++ Heros4.h
@@ -0,0 +1,32 @@
+#ifndef HEROS4_HPP
+#define HEROS4_HPP
+
+#include "Hero.h"
+
+#include <string>
+
+class Archaeologist : public Hero {
+public:
+    Archaeologist();
+    void specialAction(Board* board) override;
+};
+
+class Mayor : public Hero {
+public:
+    Mayor();
+    void specialAction(Board* board) override; 
+};
+
+class Scientist : public Hero {
+public:
+    Scientist();
+    void specialAction(Board* board) override; 
+};
+
+class Courier : public Hero {
+public:
+    Courier();
+    void specialAction(Board* board) override;
+};
+
+#endif
